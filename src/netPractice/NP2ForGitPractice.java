6b6f69e946634ad218782aba3ccee630b2802770
Sanjeev Singh <krsk1512@gmail.com>
2020-07-27 20:13:13 +0530

    Added new line

--- src/netPractice/NP2ForGitPractice.java
+++ src/netPractice/NP2ForGitPractice.java
@@ -4,6 +4,8 @@ public class NP2ForGitPractice {
 
 	public static void main(String[] args) {
 		System.out.println("Checking for pull of new branch in git");
+		//Adding 1 more line of code
+		System.out.println("Added necessary changes");
 
 	}
 
